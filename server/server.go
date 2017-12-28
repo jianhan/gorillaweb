71805cd8812a0274c8f045964366ba415b1c6de8
Jian Han <jianhan@mail.com>
2017-12-28 23:08:43 +1000

    added auth0 configs, setup check auth func

--- server/server.go
+++ server/server.go
@@ -5,12 +5,38 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/spf13/viper"
 )
+
+type auth0Configs struct {
+	domain       string
+	clientID     string
+	clientSecret string
+	audience     string
+}
+
+func (a *auth0Configs) newAuth0Configs(domain, clientID, clientSecret, audience string) *auth0Configs {
+	return &auth0Configs{
+		domain:       domain,
+		clientID:     clientID,
+		clientSecret: clientSecret,
+		audience:     audience,
+	}
+}
+
+func (a *auth0Configs) validate() error {
+	if strings.TrimSpace(a.domain) == "" {
+	}
+}
+
+func init() {
+
+}
 
 func Run() {
 	r := AttachRouter(mux.NewRouter())
