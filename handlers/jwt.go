c39329dc18deab8092ea78a640eeca665966995d
Jian Han <jianhan@mail.com>
2018-01-01 23:42:06 +1000

    minor refactored checkJWTHandler

--- handlers/jwt.go
+++ handlers/jwt.go
@@ -1,26 +1,24 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func checkJWTHandler(
 	handler func(w http.ResponseWriter, r *http.Request),
 	jwtValidator jwtRequestValidatorScopeChecker,
 ) func(w http.ResponseWriter, r *http.Request) {
-	h := func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		err := jwtValidator.validateRequest(r)
 		if err != nil {
 			sendJSONResponse(w, http.StatusUnauthorized, newHTTPError(http.StatusUnauthorized, err.Error()))
 			return
-		} else {
-			err := jwtValidator.checkScope(r)
-			if err != nil {
-				sendJSONResponse(w, http.StatusUnauthorized, newHTTPError(http.StatusUnauthorized, err.Error()))
-				return
-			} else {
-				handler(w, r)
-			}
 		}
-
+		err = jwtValidator.checkScope(r)
+		if err != nil {
+			sendJSONResponse(w, http.StatusUnauthorized, newHTTPError(http.StatusUnauthorized, err.Error()))
+			return
+		}
+		handler(w, r)
 	}
-	return h
 }
